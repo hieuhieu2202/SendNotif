de8b13233cf5e64a89703f3d20e1fc7c8ca117e2
hieuhieu2202 <66201835+hieuhieu2202@users.noreply.github.com>
2025-09-15 02:33:47 -0700

    Remove obsolete UserName migration

--- RemoteControlApi/Migrations/20250917000000_AddDeviceUserInfo.cs
+++ RemoteControlApi/Migrations/20250917000000_AddDeviceUserInfo.cs
@@ -14,23 +14,12 @@ namespace RemoteControlApi.Migrations
                 type: "nvarchar(100)",
                 maxLength: 100,
                 nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "UserName",
-                table: "Devices",
-                type: "nvarchar(100)",
-                maxLength: 100,
-                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropColumn(
                 name: "CardCode",
-                table: "Devices");
-
-            migrationBuilder.DropColumn(
-                name: "UserName",
                 table: "Devices");
         }
     }
